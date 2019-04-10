0098de508640404eaf75681e082644da344eccd3
Yoshikazu Tanaka <yoshikazutanaka@yMac2018.local>
2019-04-11 06:38:51 +0900

    Style MemoDetailScreen

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoListScreen />
+        <View style={styles.headerSpace}>
+          <MemoDetailScreen />
+        </View>
       </View>
     );
   }
@@ -21,5 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerSpace: {
+    flex: 1,
+    width: '100%',
+    marginTop: 78,
   },
 });
--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -39,7 +39,6 @@ const styles = StyleSheet.create({
   memoList: {
     width: '100%',
     flex: 1,
-    paddingTop: 78,
   },
   memoListItem: {
     padding: 16,
